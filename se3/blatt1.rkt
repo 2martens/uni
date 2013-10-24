5e2b1503d48118b7586bc3eb8d4557c0f5e5a5be
Jim Martens <jim1@live.de>
2013-10-24 14:22:48 +0200

    SE3-1: 2.1 ansatzweise geloest (funzt noch nicht).

--- se3/blatt1.rkt
+++ se3/blatt1.rkt
@@ -32,3 +32,36 @@ Abgebende: Jim 2martens, 2noack, 0giebel
 (define (nmToKM nauticMiles)
   (* nauticMiles 1.852)
   )
+
+; 2.1
+(define (distanzAB breiteA laengeA breiteB laengeB)
+  ; Parameter in Radiant umrechnen
+  (define breiteARad (degreeToRadian breiteA))
+  (define breiteBRad (degreeToRadian breiteB))
+  ; Differenz geographische Längen
+  (define distanzLaenge 
+    (- laengeB laengeA)
+    )
+  ; Differenz in Radiant umrechnen
+  (define distanzLaengeRad (degreeToRadian distanzLaenge))
+  (define cosDG
+    ; ausrechnen von cos dG im Bogenmaß
+    (+ (*
+        (sin breiteARad)
+        (sin breiteBRad)
+        )
+       (* (cos breiteARad)
+          (cos breiteBRad)
+          (cos distanzLaengeRad)
+          )
+       )
+    )
+  ; umrechen in Kilometer
+  (nmToKM
+   ; ermitteln der Entfernung in Seemeilen
+   (* 60
+      ; umwandeln in Grad
+      (radianToDegree cosDG)
+      )
+   )
+  )
