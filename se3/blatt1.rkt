1c4d015147c4d42ed657f6af4ca36cc7157b8391
Jim Martens <jim1@live.de>
2013-10-24 12:56:43 +0200

    SE3-1: Aufgabe 1 geloest.

--- se3/blatt1.rkt
+++ se3/blatt1.rkt
@@ -1,1 +1,34 @@
 #lang racket
+
+#|
+SE 3 Funktional Blatt 1
+Abgebende: Jim 2martens, 2noack, 0giebel
+|#
+
+; 1.1
+
+(define (degreeToRadian degrees)
+  (/ (* degrees pi) 180)
+  )
+
+(define (radianToDegree radians)
+  (/ (* radians 180) pi)
+  )
+
+; 1.2
+(define (my-acos cosinus)
+  ; berechnen von alpha
+  (atan 
+   ; berechnen des tangens
+   (/
+    ; berechnen des Sinus
+    (sqrt (- 1 (sqr cosinus)))
+    cosinus
+    )
+   )
+  )
+
+; 1.3
+(define (nmToKM nauticMiles)
+  (* nauticMiles 1.852)
+  )
