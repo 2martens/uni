d958bd7614477cc2339a173275cf380b36ba4abb
Jim Martens <jim1@live.de>
2014-01-23 13:14:59 +0100

    SE3-11: Blatt 11 angefangen.

---
+++ se3/G08_B11_Jim-2martens_Britta-2noack_Jan-Simon-0giesel.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+#|
+SE 3 Funktional Blatt 11
+Abgebende: Jim 2martens, Britta 2noack, Jan-Simon 0giesel
+|#
+
+; 1)
+
+; 1.1
+(unify '(gebaeude ?Haus weiss)
+       '(gebaeude Informatikum ?Farbe) no-bindings)
+
+; 1.2
+
+; 2)
+
+; 3)
