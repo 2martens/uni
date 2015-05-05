1224e51a80c5e439118fd6bc3345e770d6e834f4
Jim Martens <github@2martens.de>
2015-05-05 17:36:50 +0200

    [ES] Fixed wrong boundaries
    Signed-óff-by: Jim Martens <github@2martens.de>

--- es/blatt3/uebung3-2/uebung3-2.ino
+++ es/blatt3/uebung3-2/uebung3-2.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <Servo>
 
 // these variables describe the used hardware pins
 // adjust them when you use other pins
@@ -10,7 +10,7 @@ Servo ourServo;
 
 // used to achieve a 10 Hz frequency
 // don't touch them
-int rc = 1049999;
+long rc = 1049999;
 
 /**
  * Setup function for initial setup code
@@ -19,8 +19,6 @@ void setup() {
   // Configure servo
   ourServo.attach(servoPin);
   ourServo.write(90);
-  
-  pinMode(servoPin, OUTPUT);
   
   // initialize serial port
   Serial.begin(9600);
@@ -35,12 +33,12 @@ void loop() {
     delay(10);
   }
   delay(1000);
-  for (int i = 180; i >= -180; --i) {
+  for (int i = 180; i >= 0; --i) {
     ourServo.write(i);
     delay(10);
   }
   delay(1000);
-  for (int i = -180; i <= 90; ++i) {
+  for (int i = 0; i <= 90; ++i) {
     ourServo.write(i);
     delay(10);
   }
