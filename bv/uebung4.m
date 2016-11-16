cfcee956bc1a08d0f8e3ebbba97a6182f6e9173c
Jim Martens <github@2martens.de>
2016-11-16 13:56:25 +0100

    [BV] Aufgabe 2 aktualisiert
    Signed-off-by: Jim Martens <github@2martens.de>

--- bv/uebung4.m
+++ bv/uebung4.m
@@ -1,17 +1,12 @@
 % 2.
 fingerprint = imread('fingerprint.png');
-rec0 = strel('rectangle', [2, 2]);
-lin1 = strel('line', 3, 0);
-disk = strel('disk', 1, 4);
-rec1 = strel('rectangle', [3 5]);
-rec2 = strel('rectangle', [2, 2]);
-opened1 = imopen(fingerprint, lin1);
-opened2 = imopen(opened1, rec0);
-opened3 = imopen(opened2, disk);
+rec = strel('rectangle', [2 2]);
+lin = strel('line', 4, 0);
+opened = imopen(fingerprint, rec);
+opened2 = imopen(opened, lin);
 figure (1), imshow(fingerprint);
-figure (2), imshow(opened1);
+figure (2), imshow(opened);
 figure (3), imshow(opened2);
-figure (4), imshow(opened3);
 
 % 3.
 img = logical([1 0 0 0 1 1 1 0 1 1]);
