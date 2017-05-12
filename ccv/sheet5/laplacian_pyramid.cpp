6bcf879da7f5797345339ccf7fd538dfc3222a44
Jim Martens <github@2martens.de>
2017-05-12 15:11:09 +0200

    [CVV] Fixed laplacian pyramid
    Signed-off-by: Jim Martens <github@2martens.de>

--- ccv/sheet5/laplacian_pyramid.cpp
+++ ccv/sheet5/laplacian_pyramid.cpp
@@ -6,6 +6,6 @@ laplacian_pyramid::laplacian_pyramid(const gauss_pyramid &pyramid, float sigma) 
   for (int i = 0; i < number_of_layers; i++) {
     cv::Mat blurred;
     cv::GaussianBlur(pyramid.get(i), blurred, cv::Size(), sigma, sigma, cv::BORDER_CONSTANT);
-    _layers.push_back(blurred);
+    _layers.push_back(pyramid.get(i) - blurred);
   }
 }
