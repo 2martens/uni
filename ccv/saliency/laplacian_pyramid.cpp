feae7ddeaf1a0b00ca26503d3fe1e2563d9ebc3b
Jim Martens <github@2martens.de>
2017-05-30 20:51:11 +0200

    [CCV] Prevented unintended changes
    Signed-off-by: Jim Martens <github@2martens.de>

--- ccv/saliency/laplacian_pyramid.cpp
+++ ccv/saliency/laplacian_pyramid.cpp
@@ -6,7 +6,8 @@ laplacian_pyramid::laplacian_pyramid(const gauss_pyramid &pyramid, float sigma) 
   for (int i = 0; i < number_of_layers; i++) {
     cv::Mat blurred;
     cv::GaussianBlur(pyramid.get(i), blurred, cv::Size(), sigma, sigma, cv::BORDER_CONSTANT);
-    _layers.push_back(pyramid.get(i) - blurred);
+    cv::Mat laplacian = pyramid.get(i) - blurred;
+    _layers.push_back(laplacian.clone());
   }
 }
 
